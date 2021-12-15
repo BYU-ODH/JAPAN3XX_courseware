fae2200b082312f041a5011af685d66db770f3fb
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2021-12-14 17:07:33 -0700

    multiple stories per lesson

--- 301/build_html.py
+++ 301/build_html.py
@@ -42,12 +42,6 @@ with open('../common/main_front_matter.html') as f:
 
 def generate_lesson(lesson_soup, make_grammar=True, make_translation=True):
     lesson = 'lesson' + lesson_soup['lessonNumber'].zfill(2)
-    sound_file = lesson_soup.STORY['soundPath']
-    title = lesson_soup.STORY['japaneseTitle']
-    try:
-        eng_title = lesson_soup.STORY['englishTitle']
-    except KeyError:
-        eng_title = ''
     vocab_dict = {}
     for vocabulary in lesson_soup.find_all('VOCABULARY'):
         for link in vocabulary.find_all('LINK'):
@@ -88,7 +82,7 @@ def generate_lesson(lesson_soup, make_grammar=True, make_translation=True):
     kanji_json = json.dumps(kanji_dict)
     with open(f'{target_dir}/{lesson}.html', 'w') as f:
         print(f'{lesson_front_matter}\n'
-              f'        <span>{title}</span>\n'
+              f'        <span>{" &nbsp;/ &nbsp;".join(story["japaneseTitle"] for story in lesson_soup.find_all("STORY"))}</span>\n'
                '      </a>\n'
                '    </li>\n'
                '  </ul>\n'
@@ -98,88 +92,97 @@ def generate_lesson(lesson_soup, make_grammar=True, make_translation=True):
               f'  var grammar_json = {grammar_json};\n'
               f'  var kanji_json = {kanji_json};\n'
               f'  </script>\n'
-              f'  <div class="block title">{title}</div>\n'
-               '  <hr>\n'
-               '  <div class="block is-centered">\n'
-               '    <p>\n'
-               '      Listen to Whole Story <br>\n'
-               '      <audio controls controlsList="nodownload">\n'
-              f'        <source src="{sound_file}" type="audio/mpeg">\n'
-               '      </audio>\n'
-               '      </a>\n'
-               '    </p>\n'
-               '  </div>\n'
-               '  <p><b>Instructions: </b>Use the <i class="fas fa-play"></i> button to listen to a sentence.',
-              file=f)
+               '  <p><b>Instructions: </b>Use the <i class="fas fa-play"></i> button to listen to a sentence.', file=f)
         if make_translation:
-            print(f'                          Use the <i class="fas fa-language"></i> button to show a translation.<br><br>\n',
+            print(f'                      Use the <i class="fas fa-language"></i> button to show a translation.<br><br>\n',
                   file=f)
-        print(f'  <table id="sentences" class="table-NOT"><tbody>\n',
-              file=f)
-        for line in lesson_soup.STORY.find_all('LINE'):
+        print(f'  </p>', file=f)
+        for story in lesson_soup.find_all('STORY'):
+            sound_file = story['soundPath']
+            title = story['japaneseTitle']
             try:
-                sent_id_a, sent_id_b = re.search(r'^sound/(.*)[-_](.*)\.mp3$', line['soundPath']).groups()
-            except AttributeError as e:
-                print("SOUND", line)
-                raise AttributeError(f'SOUND {line}') from e
-            sent_id = f'{sent_id_a}-{sent_id_b}'
-            sound_file = f'sound/{sent_id}.mp3'
-            sent = re.sub(r'\s+', '', line.LINK.get_text())
-            trans = line['translation']
-            trans = trans.replace('"', '&quot;').replace("'", '&#39;')
-            vocab_sent = ''
-            cursor = 0
-            for match in re.finditer(vocab_re, sent):
-                vocab_sent += sent[cursor:match.start()]
-                vocab_sent += f'''<a onclick="show_vocab_data('{match.group()}')">{match.group()}</a>'''
-                cursor = match.end()
-            vocab_sent += sent[cursor:None]
-            grammar_sent = ''
-            cursor = 0
-            if make_grammar:
-                for match in re.finditer(grammar_re, sent):
-                    grammar_sent += sent[cursor:match.start()]
-                    grammar_sent += f'''<a onclick="show_grammar_data('{match.group()}')">{match.group()}</a>'''
-                    cursor = match.end()
-                grammar_sent += sent[cursor:None]
-            kanji_sent = sent
-            for kanji, (kun, on, kanji_eng, examples) in kanji_dict.items():
-                kanji_sent = kanji_sent.replace(kanji, f'''<a onclick="show_kanji_data('{kanji}')">{kanji}</a>''')
-            print(f'    <tr id="{sent_id}" class="sentence">\n'
-                   '      <td style="vertical-align: middle">\n'
-                   '      <button class="button is-rounded" onclick="aud_play_pause(this)">\n'
-                   '      <span class="icon is-clickable is-small">\n'
-                  f'        <i id="play_{sent_id}" class="fas fa-play"></i>\n'
-                  f'        <audio id="audio_{sent_id}" onended="unplay(this.parentElement.getElementsByTagName(\'i\')[0])">\n'
-                  f'          <source src="{sound_file}" type="audio/mpeg">\n'
-                   '        </audio>\n'
-                   '      </span>\n'
-                   '      </button>\n'
-                   '      </td>\n'
-                   '      <td style="vertical-align: middle">\n'
-                  f'        <span class="content-sent grammar-sent is-hidden">{grammar_sent.strip() or sent}</span>\n'
-                  f'        <span class="content-sent kanji-sent is-hidden">{kanji_sent.strip() or sent}</span>\n'
-                  f'        <span class="content-sent plain-sent is-hidden">{sent}</span>\n'
-                  f'        <span class="content-sent vocab-sent">{vocab_sent.strip() or sent}</span>\n'
-                   '      </td>\n'
-                   '    </tr>\n',
+                eng_title = story['englishTitle']
+            except KeyError:
+                eng_title = ''
+
+            print(f'  <div class="block title has-text-centered">{title}</div>\n'
+                   '  <hr>\n'
+                   '  <div class="block has-text-centered">\n'
+                   '    <p>\n'
+                   '      Listen to Whole Story <br>\n'
+                   '      <audio controls controlsList="nodownload">\n'
+                  f'        <source src="{sound_file}" type="audio/mpeg">\n'
+                   '      </audio>\n'
+                   '      </a>\n'
+                   '    </p>\n'
+                   '  </div>\n',
                   file=f)
-            if make_translation:
-                print(f'    <tr>\n'
+            print(f'  <table id="sentences" class="table-NOT"><tbody>\n',
+                  file=f)
+            for line in story.find_all('LINE'):
+                try:
+                    sent_id_a, sent_id_b = re.search(r'^sound/(.*)[-_](.*)\.mp3$', line['soundPath']).groups()
+                except AttributeError as e:
+                    print("SOUND", line)
+                    raise AttributeError(f'SOUND {line}') from e
+                sent_id = f'{sent_id_a}-{sent_id_b}'
+                sound_file = f'sound/{sent_id}.mp3'
+                sent = re.sub(r'\s+', '', line.LINK.get_text())
+                trans = line['translation']
+                trans = trans.replace('"', '&quot;').replace("'", '&#39;')
+                vocab_sent = ''
+                cursor = 0
+                for match in re.finditer(vocab_re, sent):
+                    vocab_sent += sent[cursor:match.start()]
+                    vocab_sent += f'''<a onclick="show_vocab_data('{match.group()}')">{match.group()}</a>'''
+                    cursor = match.end()
+                vocab_sent += sent[cursor:None]
+                grammar_sent = ''
+                cursor = 0
+                if make_grammar:
+                    for match in re.finditer(grammar_re, sent):
+                        grammar_sent += sent[cursor:match.start()]
+                        grammar_sent += f'''<a onclick="show_grammar_data('{match.group()}')">{match.group()}</a>'''
+                        cursor = match.end()
+                    grammar_sent += sent[cursor:None]
+                kanji_sent = sent
+                for kanji, (kun, on, kanji_eng, examples) in kanji_dict.items():
+                    kanji_sent = kanji_sent.replace(kanji, f'''<a onclick="show_kanji_data('{kanji}')">{kanji}</a>''')
+                print(f'    <tr id="{sent_id}" class="sentence">\n'
                        '      <td style="vertical-align: middle">\n'
-                       '        <button class="button is-rounded" onclick="show_hide_trans(this)">\n'
-                      f'        <span class="icon">\n'
-                      f'          <i class="fas fa-language"></i>\n'
-                       '        </span>\n'
-                       '        </button>\n'
+                       '      <button class="button is-rounded" onclick="aud_play_pause(this)">\n'
+                       '      <span class="icon is-clickable is-small">\n'
+                      f'        <i id="play_{sent_id}" class="fas fa-play"></i>\n'
+                      f'        <audio id="audio_{sent_id}" onended="unplay(this.parentElement.getElementsByTagName(\'i\')[0])">\n'
+                      f'          <source src="{sound_file}" type="audio/mpeg">\n'
+                       '        </audio>\n'
+                       '      </span>\n'
+                       '      </button>\n'
                        '      </td>\n'
                        '      <td style="vertical-align: middle">\n'
-                      f'        <span class="trans is-invisible">{trans}</span>\n'
+                      f'        <span class="content-sent grammar-sent is-hidden">{grammar_sent.strip() or sent}</span>\n'
+                      f'        <span class="content-sent kanji-sent is-hidden">{kanji_sent.strip() or sent}</span>\n'
+                      f'        <span class="content-sent plain-sent is-hidden">{sent}</span>\n'
+                      f'        <span class="content-sent vocab-sent">{vocab_sent.strip() or sent}</span>\n'
                        '      </td>\n'
                        '    </tr>\n',
                       file=f)
-        print('  </tbody></table>\n'
-              '  <nav class="navbar has-shadow is-fixed-bottom is-light is-spaced">\n'
+                if make_translation:
+                    print(f'    <tr>\n'
+                           '      <td style="vertical-align: middle">\n'
+                           '        <button class="button is-rounded" onclick="show_hide_trans(this)">\n'
+                          f'        <span class="icon">\n'
+                          f'          <i class="fas fa-language"></i>\n'
+                           '        </span>\n'
+                           '        </button>\n'
+                           '      </td>\n'
+                           '      <td style="vertical-align: middle">\n'
+                          f'        <span class="trans is-invisible">{trans}</span>\n'
+                           '      </td>\n'
+                           '    </tr>\n',
+                          file=f)
+            print('  </tbody></table>\n', file=f)
+        print('  <nav class="navbar has-shadow is-fixed-bottom is-light is-spaced">\n'
               '      <div class="field has-addons">\n'
               '        <p class="control">\n'
               '          <button class="button is-primary mode-button vocab" onclick="change_mode(\'vocab\')">Vocab</button>\n'
@@ -210,7 +213,7 @@ def generate_main(soup):
                 unit = 'Lessons'
             short_lesson = lesson['lessonNumber'].zfill(2)
             full_lesson = 'lesson' + short_lesson
-            title = lesson.STORY['japaneseTitle']
+            title = '&nbsp; &nbsp;/&nbsp; &nbsp;'.join(story['japaneseTitle'] for story in lesson.find_all('STORY'))
             try:
                 eng_title = lesson.STORY['englishTitle']
             except KeyError:
